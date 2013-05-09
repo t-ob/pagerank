8748ff0162130ab5d99a41a3f48990a50100208a
Tom O'Brien <thomas.obrien86@gmail.com>
2013-05-09 21:00:04 +0100

    Calculate initial input vector from source

--- src/pagerank/core.clj
+++ src/pagerank/core.clj
@@ -3,6 +3,16 @@
             [cascalog.ops :as c]
             [pagerank.normalise :refer [norm]]))
 
+(defn instantiate-vector [source]
+  (let [c (ffirst (??<- [?c]
+                        (source ?a _ _)
+                        (c/distinct-count :> ?c)))
+        r (double (/ 1 c))]
+    (<- [?a ?v]
+        (source ?a _ _)
+        (identity r :> ?v)
+        (:distinct :true))))
+
 (defn pr-iter [source vs]
   (<- [?i ?s]
       (source ?j ?i ?a)
